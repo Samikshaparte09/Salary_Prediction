7c56a1d31ebbd4f75cb0094267219375ebfd0a1c
Samikshaparte09 <samikshaparte79@gmail.com>
2026-08-19 11:22:25 +0530

    Fix model file name case in deployment script

--- salary_prediction_deployment.py
+++ salary_prediction_deployment.py
@@ -12,7 +12,7 @@ import pandas as pd
 import numpy as np
 import joblib
 
-model = joblib.load("Salary_prediction_model.pkl")
+model = joblib.load("salary_prediction_model.pkl")
 encoder = joblib.load("label_encoder.pkl")
 
 st.title("Salary Prediction")
