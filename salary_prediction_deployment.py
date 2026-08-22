f9bc243692e9bb9c4f3ada716057142793b946c3
Samikshaparte09 <samikshaparte79@gmail.com>
2026-08-22 11:02:55 +0530

    Update salary_prediction_deployment.py

--- salary_prediction_deployment.py
+++ salary_prediction_deployment.py
@@ -12,7 +12,7 @@ import pandas as pd
 import numpy as np
 import joblib
 
-model = joblib.load("Salary_prediction_model (1).pkl")
+model = joblib.load("salary_prediction_model (1).pkl")
 encoder = joblib.load("label_encoder (1).pkl")
 
 st.title("Salary Prediction")
